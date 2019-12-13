56da8616ad19e8c2b3630f185264bd3e94833308
Osvaldo G Del Rio <osdelrio@hotmail.com>
2019-12-12 20:27:30 -0800

    Fix
    Adeudo

--- app/modelos/aportaciones/BuscarAdeudoPacientePorNING_ID.php
+++ app/modelos/aportaciones/BuscarAdeudoPacientePorNING_ID.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\modelos\aportaciones;
 use \app\modelos\sql\{DBGet, DBResultCount, DBResultObj};
-use \app\modelos\pacientes\{ContarDiasTratamientoPacienteporNING_ID,ObtenerConsentimientoInformadoPorNING_ID};
+use \app\modelos\pacientes\{ContarDiasTratamientoPacienteporNING_ID};
 use \app\modelos\aportaciones\ObtenerAportacionesPacienteporNING_ID;
 /**
  * 
@@ -14,7 +14,6 @@ class BuscarAdeudoPacientePorNING_ID
 		DBResultCount $DBResultCount, 
 		DBResultObj $DBResultObj,
 		ContarDiasTratamientoPacienteporNING_ID $ContarDiasTratamientoPacienteporNING_ID,
-		ObtenerConsentimientoInformadoPorNING_ID $ObtenerConsentimientoInformadoPorNING_ID,
 		ObtenerAportacionesPacienteporNING_ID $ObtenerAportacionesPacienteporNING_ID	
 	)
 	{
@@ -22,74 +21,11 @@ class BuscarAdeudoPacientePorNING_ID
 		$this->_count = $DBResultCount;
 		$this->_result = $DBResultObj;
 		$this->_dias = $ContarDiasTratamientoPacienteporNING_ID;
-		$this->_consentimiento = $ObtenerConsentimientoInformadoPorNING_ID;
 		$this->_aportaciones = $ObtenerAportacionesPacienteporNING_ID;
 	}
 
 	public function obtener($ning_id)
 	{
-		if($consentimiento = $this->_consentimiento->obtener($ning_id))
-		{
-			if($consentimiento->BECA !== '1')
-			{
-				if($consentimiento->PERIODO == '1')
-				{
-					$dias = $this->_dias->obtener($ning_id);
-					
-					if($dias > 21)
-					{
-						if($aportaciones = $this->_aportaciones->obtener($ning_id))
-						{
-							$limiteDeuda = $consentimiento->CANTIDAD * 3;
-							$PagoSinAdeudo = (floor($dias / 7) + 1) * $consentimiento->CANTIDAD;
-							
-							$PeriodicaPesos = 0;
-							$PeriodicaDolares = 0;
-							
-							foreach ($aportaciones as $key => $value)
-							{
-								if($value->MONEDA == 'pesos')
-								{
-									$PeriodicaPesos = $PeriodicaPesos + $value->CANTIDAD;
-								}
-								else
-								{
-									$PeriodicaDolares = $PeriodicaDolares + $value->CANTIDAD;
-								}
-							}
-							
-							if($PeriodicaDolares > 0)
-							{
-								$PeriodicaDolares = $PeriodicaDolares * (18.50);
-							}
-
-							$totalPagado = $PeriodicaPesos + $PeriodicaDolares;
-
-							if(($PagoSinAdeudo - $totalPagado) >= $limiteDeuda)
-							{
-								return $atraso = array('CANTIDAD' => $PagoSinAdeudo - $totalPagado, 'FORMA' => 'Periódicas', 'DIAS' => $dias);
-							}
-						}
-						else
-						{
-							// NO HA DADO APORTACIONES EN MAS DE UN MES
-							return $atraso = array
-							('CANTIDAD' => $PagoSinAdeudo = (floor($dias / 7)) * $consentimiento->CANTIDAD,
-							 'FORMA' => 'Periódicas', 'DIAS' => $dias);
-						}
-
-					}
-				}
-				else
-				{
-					//MENSUAL
-
-				}
-				
-			}// NO HA PASADO UN MES
-			
-		}
-
-		return false;
+		
 	}
 }
